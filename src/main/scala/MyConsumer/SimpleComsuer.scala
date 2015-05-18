005485ab4c27258494f3057f99fa8e1f0997b077
juniuszhou <junius.zhou@gmail.com>
2015-05-18 22:53:23 +0800

    producer is ok

---
+++ src/main/scala/MyConsumer/SimpleComsuer.scala
@@ -0,0 +1,10 @@
+package MyConsumer
+
+/**
+ * Created by junius on 15-5-18.
+ */
+object SimpleComsuer {
+  def main (args: Array[String]) {
+
+  }
+}
--- src/main/scala/MyProducer/SimpleProducer.scala
+++ src/main/scala/MyProducer/SimpleProducer.scala
@@ -10,25 +10,19 @@ import scala.util.Random
 object SimpleProducer {
   def main(args: Array[String]) = {
 
-    val r = new Random()
     val props = new java.util.Properties()
-    props.put("metadata.broker.list", "localhost:8888")
-    props.put("serializer.class", "kafka.serializer.StringEncoder")
-    //props.put("partitioner.class", "example.producer.SimplePartitioner")
-    props.put("request.required.acks", "1")
+    props.put("metadata.broker.list", "localhost:9092")
     val config = new ProducerConfig(props)
 
-    val prod = new Producer[String,String](config)
-    (0 until 100).foreach(i => {
-      val ip = "127.0.0.1"
-      val msg = " I am " + i + "th message"
-      val data = new KeyedMessage[String, String]("topic one", ip, msg)
+    val prod = new Producer[String, String](config)
 
-        prod.send(data)
+    val topic = "test"
 
-    })
+    val key = "127.0.0.1"
+    val msg = " I am the message"
+    val data = new KeyedMessage[String, String](topic, key, msg)
 
-
+    prod.send(data)
     prod.close
   }
 }
